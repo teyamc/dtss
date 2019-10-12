f3e767bf97adb247156e95f152c7204c63a69477
sunsara <sunsara@cs.washington.edu>
2019-10-12 16:39:00 -0700

    Node Class Improved

--- app/src/main/java/com/example/organizer/Node.java
+++ app/src/main/java/com/example/organizer/Node.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 // Node class for the to-do lists
 public class Node<String> {
-    public String value; // The text stored in the item
-    public Date date; // The date of the to-do item
-    public Set<String> tag;
-    public Node<String> next;
+    private int id; // ID of the node
+    private String value; // The text stored in the item
+    private Date date; // The date of the to-do item
+    private Set<String> tag; // The tag of the to-do item
+    private Node<String> next; // The next item in the to-do list
 
     // Creates a node given a value
     public Node(String value) {
@@ -31,5 +32,25 @@ public class Node<String> {
     // Not yet implemented
     public void setTag(Node item, Set<String> tag) {
 
+    }
+
+    // Returns the value of the node
+    public String getValue() {
+        return this.value;
+    }
+
+    // Returns the date of the node
+    public Date getDate() {
+        return this.date;
+    }
+
+    // Returns the tag of the node
+    public Set<String> getTag() {
+        return this.tag;
+    }
+
+    // Returns the next of the node
+    public Node<String> getNext() {
+        return this.next;
     }
 }
