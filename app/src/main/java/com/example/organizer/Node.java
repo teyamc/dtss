70b7cc8137306669a04ea035e8aa2a91187c1b15
Donna Albee <albeed@uw.edu>
2019-10-12 20:10:50 -0700

    merged

--- app/src/main/java/com/example/organizer/Node.java
+++ app/src/main/java/com/example/organizer/Node.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 // Node class for the to-do lists
 public class Node {
-    private int id; // ID of the node
-    private int order;
+    private long id; // ID of the node
     private String value; // The text stored in the item
     private String date; // The date of the to-do item
     private Set<String> tag; // The tag of the to-do item
@@ -13,13 +12,11 @@ public class Node {
     // Creates a node given a value
     public Node(String value, int order) {
         this.value = value;
-        this.order = order;
     }
 
     // Creates an empty node
     public Node() {
         this.value = null;
-        this.order = -1;
     }
 
     // Sets the value of the given item to the given item
@@ -27,9 +24,6 @@ public class Node {
         item.value = value;
     }
 
-    public void setOrder(int newOrder){
-        this.order = newOrder;
-    }
     // Sets the date of the given item to the given date
     public void setDate(Node item, String date) {
         item.date = date;
@@ -42,11 +36,10 @@ public class Node {
     // Sets the next given the item and the next item
     public void setNext(Node item, Node next) {
         item.next = next;
-        item.next.setOrder(order + 1);
     }
 
     // Returns the id of the node
-    public int getId() { return this.id; }
+    public long getId() { return this.id; }
 
     // Returns the value of the node
     public String getValue() {
@@ -56,10 +49,6 @@ public class Node {
     // Returns the date of the node
     public String getDate() {
         return this.date;
-    }
-
-    public int getOrder(){
-        return this.order;
     }
 
     // Returns the tag of the node
