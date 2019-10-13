ec800930ace8caffbd227f154c12d89d3b9aef2a
Donna Albee <albeed@uw.edu>
2019-10-12 21:15:02 -0700

    Changed ui

--- app/src/main/java/com/example/organizer/MasterList.java
+++ app/src/main/java/com/example/organizer/MasterList.java
@@ -1,5 +1,28 @@
 package com.example.organizer;
 
 public class MasterList {
+    public ListNode front;
 
+    public MasterList(){
+        front = new ListNode();
+    }
+
+    public void addList(ToDoList list){
+
+    }
+
+    public class ListNode {
+        public ToDoList data;
+        public ListNode next;
+
+        public ListNode(){
+            this.data = null;
+            this.next = null;
+        }
+
+        public ListNode(ToDoList data){
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
