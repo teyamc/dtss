135d32e3cd0f33a7d0ed5c74dc49c213b7f0da55
sunsara <sunsara@cs.washington.edu>
2019-10-13 10:06:13 -0700

    List works??!!?

--- app/src/main/java/com/example/organizer/MainActivity.java
+++ app/src/main/java/com/example/organizer/MainActivity.java
@@ -6,13 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    public ToDoList list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        list = new ToDoList();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -23,11 +25,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText txtname = findViewById(R.id.input_text);
                 String name = txtname.getText().toString();
+                makeListItem(name);
+                Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
             }
         });
     }
 
     public void makeListItem(String value) {
-
+        list.add(new Node(value));
     }
 }
